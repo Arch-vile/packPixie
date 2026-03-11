92d7bfc98dcfe5d553610e044725e07c89454f24
Arch-vile <mikko.ravimo@gmail.com>
2026-03-11 14:09:22 +0200

    add pixie logo

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -5,6 +5,7 @@ import Comments from './Comments';
 function App() {
   return (
     <>
+      <img src="/pixie.png" alt="PackPixie logo" className="app-logo" />
       <h1>PackPixie</h1>
       <StatusChecker />
       <Comments />
